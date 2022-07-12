3a00caee379db477d48e1b444831a75f3248e849
Trayek <jzi@sitecore.net>
2022-07-11 22:02:33 -0400

    Some updates

--- src/sxastarter/src/components/PageBanner.tsx
+++ src/sxastarter/src/components/PageBanner.tsx
@@ -1,33 +1,8 @@
 import React from 'react';
-import { Field, ImageField, Text } from '@sitecore-jss/sitecore-jss-nextjs';
 
-export type RichTextProps = {
-  params: { [key: string]: string };
-  fields: {
-    Title: Field<string>,
-    BannerImage: ImageField
-  }
-};
-
-export const Default = (props: RichTextProps): JSX.Element => {
+export const Default = (): JSX.Element => {
   return (
     <div className="page-title-area item-bg3">
-      <div className="container">
-        <div className="page-title-content">
-          {/* <h2><Text field={props.fields.Title} /></h2> */}
-          <h2>My title</h2>
-          <ul>
-            <li>
-              <a href="/">
-                Home
-              </a>
-            </li>
-            {/* <li><Text field={props.fields.Title} /></li> */}
-            <li>Another title field</li>
-          </ul>
-        </div>
-      </div>
-
       <div className="lines">
         <div className="line"></div>
         <div className="line"></div>
