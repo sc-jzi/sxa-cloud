425dd15032d9e2b2452de1dcd6da804c9bfb39cb
Trayek <jzi@sitecore.net>
2022-07-11 21:26:58 -0400

    Another try...

--- src/sxastarter/src/components/PageBanner.tsx
+++ src/sxastarter/src/components/PageBanner.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import { Field, Text } from '@sitecore-jss/sitecore-jss-nextjs';
-
-interface Fields {
-  Title: Field<string>;
-}
+import { Field, ImageField, Text } from '@sitecore-jss/sitecore-jss-nextjs';
 
 export type RichTextProps = {
   params: { [key: string]: string };
-  fields: Fields;
+  fields: {
+    Title: Field<string>,
+    BannerImage: ImageField
+  }
 };
 
 export const Default = (props: RichTextProps): JSX.Element => {
@@ -15,14 +14,16 @@ export const Default = (props: RichTextProps): JSX.Element => {
     <div className="page-title-area item-bg3">
       <div className="container">
         <div className="page-title-content">
-          <h2><Text field={props.fields.Title} /></h2>
+          {/* <h2><Text field={props.fields.Title} /></h2> */}
+          <h2>My title</h2>
           <ul>
             <li>
               <a href="/">
                 Home
               </a>
             </li>
-            <li><Text field={props.fields.Title} /></li>
+            {/* <li><Text field={props.fields.Title} /></li> */}
+            <li>Another title field</li>
           </ul>
         </div>
       </div>
